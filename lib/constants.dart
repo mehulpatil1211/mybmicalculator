5238be660ae441b4165f39141c8688219828dd7e
asus <mehulpatil1211>
2020-07-21 17:14:56 +0530

    This Is My Bmi Calculator app

---
+++ lib/constants.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+const kBottomContainerHeight = 80.0;
+const kActiveCardColour = Color(0xFFffd600);
+const kInactiveCardColour = Color(0xFFffee58);
+const kBottomContainerColour = Color(0xFF545454);
+const kbackstylecolor = Colors.black;
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Colors.black,
+);
+
+const kNumberTextStyle = TextStyle(
+  color: Colors.black,
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
+
+const kLargeButtonTextStyle = TextStyle(
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultTextStyle = TextStyle(
+  color: Color(0xFF03B102),
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBMITextStyle = TextStyle(
+  fontSize: 100.0,
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+);
+
+const kBodyTextStyle = TextStyle(
+  fontSize: 22.0,
+  color: Colors.black,
+);
